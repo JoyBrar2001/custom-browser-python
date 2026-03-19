681b152aa27e2e50b7c74417bedea0065b446c5e
Joy <joybrar2001@gmail.com>
2026-03-19 05:48:21 +0530

    Added back and forward buttons

--- main.py
+++ main.py
@@ -4,8 +4,33 @@ from PyQt5.QtCore import QUrl
 
 app = QApplication([])
 
+# Main Window
+window = QWidget()
+layout = QVBoxLayout()
+
+# Nav bar
+nav_bar = QHBoxLayout()
+
+back_btn = QPushButton("⬅ Back")
+forward_btn = QPushButton("Forward ➡")
+
+nav_bar.addWidget(back_btn)
+nav_bar.addWidget(forward_btn)
+
+# Browser
 browser = QWebEngineView()
 browser.setUrl(QUrl("https://www.google.com"))
 
-browser.show()
+# Connect Buttons
+back_btn.clicked.connect(browser.back)
+forward_btn.clicked.connect(browser.forward)
+
+# Add to layout
+layout.addLayout(nav_bar)
+layout.addWidget(browser)
+
+window.setLayout(layout)
+window.resize(1000, 700)
+window.show()
+
 app.exec()
